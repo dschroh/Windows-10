92059efa4a4426bf63cc90719295f519f6793ae9
David Schroh <dschroh@uncharted.software>
2015-08-24 14:20:38 -0400

    Update Windows10-SysPrep.ps1
    Tweaked for non-fresh install

--- Windows10-SysPrep.ps1
+++ Windows10-SysPrep.ps1
@@ -30,10 +30,10 @@ Remove-Item -Path 'HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}' -Recurse
 Remove-Item -Path 'HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}' -Recurse
 
 ## Kill access to the Windows Store
-If (-Not (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore")) {
-	New-Item -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" | Out-Null
-}
-New-ItemProperty -Force -Path HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore -Name RemoveWindowsStore -Type DWord -Value 1
+#If (-Not (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore")) {
+#	New-Item -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" | Out-Null
+#}
+#New-ItemProperty -Force -Path HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore -Name RemoveWindowsStore -Type DWord -Value 1
 
 ## Block connection to Microsoft Accounts
 If (-Not (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) {
@@ -58,10 +58,10 @@ If (-Not (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOpt
 New-ItemProperty -Force -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config -Name DODownloadMode -Type DWord -Value 0
 
 # Require Ctrl-Alt-Del to log on
-If (-Not (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System")) {
-	New-Item -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" | Out-Null
-}
-New-ItemProperty -Force -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name DisableCAD -Type DWord -Value 0
+#If (-Not (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System")) {
+#	New-Item -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" | Out-Null
+#}
+#New-ItemProperty -Force -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name DisableCAD -Type DWord -Value 0
 
 # Block "Add features to Windows 10"
 If (-Not (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\WAU")) {
@@ -136,44 +136,4 @@ New-ItemProperty -Force -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsU
 $modernApps = @("Microsoft.Reader","Microsoft.WindowsReadingList","Microsoft.3DBuilder","microsoft.windowscommunicationsapps","Microsoft.BingFinance","Microsoft.BingNews","Microsoft.BingSports","Microsoft.BingWeather","Microsoft.BingTravel","Microsoft.BingHealthAndFitness","Microsoft.BingFoodAndDrink","Microsoft.People","Microsoft.WindowsPhone","Microsoft.MicrosoftSolitaireCollection","Microsoft.WindowsSoundRecorder","Microsoft.XboxApp","Microsoft.WindowsCamera","Microsoft.ZuneMusic","Microsoft.ZuneVideo","Microsoft.Office.OneNote","Microsoft.SkypeApp","Microsoft.MicrosoftOfficeHub")
 foreach ($modernApp in $modernApps) {
     Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -match $modernApp} | Remove-AppxProvisionedPackage -Online
-}
-
-Get-AppxPackage -AllUsers | Remove-AppxPackage
-
-#### Acquired the below from https://msdn.microsoft.com/en-us/library/windows/hardware/mt185364(v=vs.85).aspx
-
-## Get all the provisioned packages
-$Packages = (get-item 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications') | Get-ChildItem
-
-## Filter the list if provided a filter
-$PackageFilter = $args[0] 
-if ([string]::IsNullOrEmpty($PackageFilter))
-{
-	echo "No filter specified, attempting to re-register all provisioned apps."
-}
-else
-{
-	$Packages = $Packages | where {$_.Name -like $PackageFilter} 
-
-	if ($Packages -eq $null)
-	{
-		echo "No provisioned apps match the specified filter."
-		exit
-	}
-	else
-	{
-		echo "Registering the provisioned apps that match $PackageFilter"
-	}
-}
-
-ForEach($Package in $Packages)
-{
-	## get package name & path
-	$PackageName = $Package | Get-ItemProperty | Select-Object -ExpandProperty PSChildName
-	$PackagePath = [System.Environment]::ExpandEnvironmentVariables(($Package | Get-ItemProperty | Select-Object -ExpandProperty Path))
-
-	## register the package	
-	echo "Attempting to register package: $PackageName"
-
-	Add-AppxPackage -register $PackagePath -DisableDevelopmentMode                  
 }
